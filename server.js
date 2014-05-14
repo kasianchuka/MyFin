1fee2922939e676e26306f0d4011f3042fe17db6
Adrii <3.bunall@gmail.com>
2014-05-14 15:45:19 +0000

    [REQ] Add logger on server side & 1 REST endpoint

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var app = express();
+var morgan = require('morgan');
 
 app.use(express.static(__dirname));
+app.use(morgan('short'));
+app.post('/incomeData', function(request, response) {
+	response.send({ 'data': 'q23435rfdhgf43' }).status(200);
+	console.log('data posted');
+});
 
 app.listen(3000);
